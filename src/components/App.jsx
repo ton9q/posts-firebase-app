d519a1ba3e30a3cee0178aaee4b91d467138fea8
ton9q <kuchma_anton@mail.ru>
2019-03-25 14:43:21 +0300

    feat: add components for project

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Navbar from './layout/Navbar';
+import Dashboard from './dashboard/Dashboard';
+import ProjectDetails from './projects/ProjectDetails';
+import SignIn from './auth/SignIn';
+import SignUp from './auth/SignUp';
+import CreateProject from './projects/CreateProject';
+
+const App = () => (
+  <BrowserRouter>
+    <div className="App">
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/project/:id" component={ProjectDetails} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/create" component={CreateProject} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default App;
