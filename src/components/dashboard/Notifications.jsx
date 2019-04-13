5f18970e98f18afc51f3845baedbc8aa4f2e4614
ton9q <kuchma_anton@mail.ru>
2019-04-14 00:10:33 +0300

    feat(notifications): add notifications data with firebase cloud functions

--- src/components/dashboard/Notifications.jsx
+++ src/components/dashboard/Notifications.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import moment from 'moment';
 
-const Notifications = () => {
+const Notifications = (props) => {
+  const { notifications } = props;
+
   return (
-    <div>
-      <p>Notifications</p>
+    <div className="section">
+      <div className="card z-dedpth-0">
+        <div className="card-content">
+          <div className="card-title">Notifications</div>
+          <ul className="notifications">
+            {notifications
+              && notifications.map(notification => (
+                  <li key={notification.id}>
+                    <span className="pink-text">{notification.user}</span>
+                    {' '}
+                    <span>{notification.content}</span>
+                    <div className="grey-text note-date">
+                      {moment(notification.time.toDate()).fromNow()}
+                    </div>
+                  </li>
+              ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
